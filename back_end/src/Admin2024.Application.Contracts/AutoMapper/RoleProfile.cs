417e6234a0653e5505bd7941a7d26d0fdf80804d
连增钱 <2071260354@qq.com>
2024-07-23 09:30:28 +0800

    雪芳角色功能

---
+++ back_end/src/Admin2024.Application.Contracts/AutoMapper/RoleProfile.cs
@@ -0,0 +1,13 @@
+﻿using Admin2024.Application.Contracts.RoleApplication.Dto;
+using Admin2024.Domain.System;
+using AutoMapper;
+
+namespace Admin2024.Application.Contracts.AutoMapper;
+public class RoleProfile : Profile
+{
+    public RoleProfile()
+    {
+        CreateMap<RoleCreateInfoDto, Role>();
+        CreateMap<RoleUpdateInfoDto, Role>();
+    }
+}
