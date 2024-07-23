96f8a6d2d4e5af127e77828f8d93fa0c1543fd56
Your Name <you@example.com>
2024-07-23 16:03:20 +0800

    asld

---
+++ front_end/admin2024/src/views/userrole.vue
@@ -0,0 +1,316 @@
+<template>
+    <div class="el">
+        <!-- //el-left 页面左侧代码布局 -->
+        <div class="el-left">
+            <a-menu class="el-left-menu" @click="handleClick">
+                <a-menu-item key="0" disabled>
+                    角色名称
+                </a-menu-item>
+                <a-menu-item key="1">
+                    系统管理员
+                </a-menu-item>
+
+            </a-menu>
+        </div>
+        <!-- //el-right 页面右侧代码布局 -->
+        <div class="el-right">
+            <button type="button" class="el-right-button" style="background-color: rgb(24, 144, 255);"
+                @click="Add">添加用户</button>
+            <button type="button" class="el-right-button" style="background-color: rgb(255,73,73);"
+                @click="cancels">全部取消</button>
+            <span class="el-right-icon">
+                <SearchOutlined />
+            </span>
+            <input type="text" placeholder="请输入关键字">
+            <table class="el-right-table">
+                <tr>
+                    <th><input type="checkbox" name="" id=""></th>
+                    <th>用户账号</th>
+                    <th>用户名称</th>
+                    <th>性别</th>
+                    <th>邮箱</th>
+                    <th>是否启用</th>
+                    <th>备注</th>
+                </tr>
+                <tr v-for="item in tabArr">
+                    <td><input type="checkbox" name="" :id="item.id.toString()"></td>
+                    <td>{{ item.account }}</td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.gender }}</td>
+                    <td>{{ item.email }}</td>
+                    <td>
+                        <div v-if="item.checked == true">
+                            <CheckCircleFilled style="color: green;" />
+                        </div>
+                        <div v-else-if="item.checked == false">
+                            <CloseCircleFilled style="color: red;" />
+                        </div>
+                    </td>
+                    <td>{{ item.remark }}</td>
+                </tr>
+            </table>
+        </div>
+        <!-- //el-dialog 模态框-->
+        <div class="el-dialog" v-show="dialog">
+            <div class="el-dialog-content">
+                <div><label>添加用户</label></div>
+                <span class="el-dialog-icon">
+                    <SearchOutlined />
+                </span>
+                <input type="text" placeholder="请输入关键字">
+
+                <a-table class="a-table" :row-selection="rowSelection" :columns="columns" :data-source="data"
+                    :pagination="{ pageSize: 50 }" :scroll="{ x: 700, y: 450 }" bordered />
+                <div class="el-dialog-footer">
+                    <button type="button" class="button1" @click=confirm>确定</button>
+                    <button type="button" class="button2" @click="cancel">取消</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script setup>
+import { ref, reactive } from "vue";
+
+import {
+    SearchOutlined,
+    CheckCircleFilled,
+    CloseCircleFilled
+} from '@ant-design/icons-vue';
+
+//模态框显示或隐藏
+let dialog = ref(false);
+//表格测试数据
+let tabArr = reactive([
+    {
+        id: 1,
+        account: 1111,
+        name: '系统管理员',
+        gender: '男',
+        email: '1111@163.com',
+        checked: true,
+        remark: ''
+    }
+])
+
+//模态框内固定表头的样式
+const columns = [
+    {
+        title: '用户账号',
+        dataIndex: 'account',
+    },
+    {
+        title: '用户名称',
+        dataIndex: 'name',
+    },
+    {
+        title: '性别',
+        dataIndex: 'gender',
+    },
+    {
+        title: '邮箱',
+        dataIndex: 'email',
+    },
+    {
+        title: '是否可用',
+        dataIndex: 'checked',
+    },
+    {
+        title: '备注',
+        dataIndex: 'remark',
+    },
+];
+// 模态框内表格数据
+let data = [
+    {
+        key: 1,
+        account: '1111',
+        name: '系统管理员',
+        gender: '男',
+        email: '1111@163.com',
+        checked: true,
+        remark: ''
+    }
+]
+
+//行选择配置
+const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+        console.log('Selected row keys:', selectedRowKeys);
+        console.log('Selected rows:', selectedRows);
+    }
+}
+function handleClick(key) {
+    if (key == 2) {
+        console.log(key);
+    }
+}
+//添加用户
+function Add() {
+    dialog.value = true;
+}
+//全部取消
+function cancels() {
+
+}
+//确定
+function confirm() {
+
+}
+//取消
+function cancel() {
+    dialog.value = false;
+}
+</script>
+
+<style scoped>
+.el,
+.el-left,
+.el-right,
+.el-left-menu {
+    height: 100%;
+}
+
+.el {
+    width: 100%;
+    min-width: 900px;
+    display: flex;
+    justify-content: space-between
+}
+
+.el-left {
+    width: 200px;
+}
+
+.el-left-menu {
+    width: 100%;
+    border: 1px solid rgb(240, 240, 240);
+}
+</style>
+
+<style scoped>
+.el-right {
+    min-width: 81%;
+}
+
+.el-right-button {
+    width: 60px;
+    height: 25px;
+    color: white;
+    border-radius: 3px;
+    margin: 0 0 0 10px;
+    font-size: 12px;
+    border: 0;
+}
+
+.el-right button[type=button]:active {
+    transform: scale(0.9);
+}
+
+.el-right-icon {
+    position: absolute;
+    z-index: 1;
+    top: 155px;
+    right: 220px
+}
+
+.el-right [type=text] {
+    height: 30px;
+    padding: 0 0 0 30px;
+    border: 1px solid rgb(240, 240, 240);
+    position: absolute;
+    right: 53px;
+}
+
+.el-right-table,
+.el-right tr {
+    width: 100%;
+    margin: 30px 0 0 0;
+    text-align: center;
+    border: 1px solid rgb(240, 240, 240);
+}
+
+.el-right-table th,
+.el-right-table td {
+    padding: 10px;
+    border: 1px solid rgb(240, 240, 240);
+}
+</style>
+
+<style scoped>
+.el-dialog {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.5);
+    top: 0;
+    left: 0;
+}
+
+.el-dialog-content {
+    width: 50%;
+    height: 70%;
+    padding: 18px;
+    position: relative;
+    background-color: white;
+}
+
+.el-dialog label {
+    font-size: 20px;
+}
+
+.el-dialog-icon {
+    position: absolute;
+    z-index: 1;
+    top: 87px;
+    right: 217px;
+    color: rgb(240, 240, 240)
+}
+
+.el-dialog [type=text] {
+    height: 30px;
+    padding: 0 0 0 30px;
+    border: 1px solid rgb(240, 240, 240);
+    position: absolute;
+    top: 80px;
+    right: 50px;
+}
+
+.a-table {
+    margin: 100px 0;
+}
+
+.el-dialog-footer {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.el-dialog-footer button {
+    width: 55px;
+    height: 25px;
+    font-size: 12px;
+    border: 0;
+    border-radius: 5px;
+    margin: 0 30px 0 0;
+}
+
+.el-dialog-footer button:active {
+    transform: scale(0.9);
+}
+
+.el-dialog-footer .button1 {
+    color: white;
+    background-color: rgb(24, 144, 255);
+}
+
+.el-dialog-footer .button2 {
+    border: 2px solid rgb(240, 240, 240);
+    background-color: white;
+}
+</style>
