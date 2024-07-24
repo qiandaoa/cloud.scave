6f085b248271c819f99211b9744d6ac53df9b389
连增钱 <11999277+lianzengqian@user.noreply.gitee.com>
2024-07-24 08:21:44 +0000

    删除文件 back_end

--- back_end/src/Admin2024.Application.Contracts/UserApplication/Dto/OperationDto/PagingOPDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Admin2024.Application.Contracts.UserApplication.Dto;
-
-public class PagingOPDto
-{
-      public int? PageNumber{get;set;}
-
-      public int? PageSize{get;set;}
-}
-
--- back_end/src/Admin2024.Application.Contracts/UserApplication/Dto/UseRoleDto/PagingRoleDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Admin2024.Application.Contracts.UserApplication.Dto;
-
-public class PagingRoleDto{
-      public int? PageNumber{get;set;}
-
-      public int? PageSize{get;set;}
-}
-
