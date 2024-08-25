d24affac86ff72eb81c9206441b3a1a0929d62a1
连增钱 <2071260354@qq.com>
2024-08-25 12:21:53 +0800

    云朵管理xitong

--- front_end/admin2024/vite.config.js
+++ front_end/admin2024/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'http://101.133.150.189:24731',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
