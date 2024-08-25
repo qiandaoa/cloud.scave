ea58cfea712a9914e908944faadfe9676606816a
连增钱 <2071260354@qq.com>
2024-08-25 12:51:02 +0800

    云朵管理系统

--- front_end/admin2024/vite.config.js
+++ front_end/admin2024/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://101.133.150.189:24731',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
+  plugins: [vue()],
 })
